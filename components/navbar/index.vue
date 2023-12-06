a8dcdbafd7a17f1a88149cd47013982a5ab2fcbd
wildy13 <wildy.simanjuntak13@gmail.com>
2023-12-06 12:42:29 +0700

    refactor(navbar): delete menu on  navbar

--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div  class="fixed top-0  left-0 w-full z-50  flex justify-between items-center px-7 py-7 bg-[#111111]">
+    <div  class="fixed top-0  left-0 w-full z-50 px-7 py-7 bg-[#111111]">
         <div  class="text-xl">MY PORTOFOLIO</div>
-        <div class="text-xl">MENU</div>
     </div>
 </template>
